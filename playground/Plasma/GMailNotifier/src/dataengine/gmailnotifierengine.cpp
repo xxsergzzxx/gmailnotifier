7f3809c55d5dfba4d154d8b70afe0ff817c8dc08
gcnweb <gcnweb@1a4cf6ec-9064-11dd-be88-6f18bdb9940a>
2008-05-30 22:29:30 +0000

    Data engine is working (though it needs some more work and tweaks)

--- playground/Plasma/GMailNotifier/src/dataengine/gmailnotifierengine.cpp
+++ playground/Plasma/GMailNotifier/src/dataengine/gmailnotifierengine.cpp
@@ -17,21 +17,43 @@
 ** 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+// Own
 #include "gmailnotifierengine.h"
+#include "gmailatomfeedparser.h"
+
+// Qt
+#include <QtCore/QUrl>
+#include <QtNetwork/QHttp>
 
 
 class GMailNotifierEngine::Private
 {
 public:
-    Private() {}
+    Private()
+        : http(new QHttp)
+        , httpReqId(0)
+    {}
     ~Private() {}
+
+    QString  request;
+    QUrl     url;
+    QHttp   *http;
+    int      httpReqId;
 }; // Private()
 
 
+/*
+** Public
+*/
 GMailNotifierEngine::GMailNotifierEngine(QObject *parent, const QVariantList &args)
     : Plasma::DataEngine(parent, args)
     , d(new Private)
 {
+    // ATOM feed URL
+    d->url.setUrl("https://mail.google.com:443/mail/feed/atom", QUrl::StrictMode);
+
+    connect(d->http, SIGNAL(requestFinished(int, bool)),
+            this, SLOT(httpRequestFinished(int, bool)));
 } // ctor()
 
 
@@ -41,11 +63,67 @@ GMailNotifierEngine::~GMailNotifierEngine()
 } // dtor()
 
 
+/*
+** Protected
+*/
+/*
+void GMailNotifierEngine::init()
+{
+} // init()
+*/
+
 bool GMailNotifierEngine::sourceRequestEvent(const QString &request)
 {
-    Q_UNUSED(request)
+    // We expect a request like "username:password"
+    QChar sep(':');
+    request.trimmed();
+    kDebug() << request;
+    if (request.startsWith(sep) || request.endsWith(sep) || request.count(sep) != 1) {
+        kDebug() << "Bad request! \"username:password\" expected.";
+        return false;
+    }
+
+    int sepPos = request.indexOf(sep);
+    QString username = request.left(sepPos);
+    QString password = request.right(request.size()-(sepPos+1));
+
+    // Prevent multiple requests for now
+    if ( d->httpReqId == 0 ) {
+        kDebug() << "Requesting data...";
+        d->request = request;
+        d->http->setHost(d->url.host(), QHttp::ConnectionModeHttps, d->url.port());
+        d->http->setUser(username, password);
+        d->httpReqId = d->http->get(d->url.path());
+    } else {
+        kDebug() << "Request pending, cannot execute now!";
+        return false;
+    }
+
+    setData(request, Plasma::DataEngine::Data());
+
     return true;
 } // sourceRequestEvent()
+
+
+/*
+** Private Q_SLOTS
+*/
+void GMailNotifierEngine::httpRequestFinished(const int &requestId, const bool &error)
+{
+    if (requestId != d->httpReqId) {
+        return;
+    }
+    if (error) {
+        qDebug() << d->http->errorString();
+        d->httpReqId = 0;
+        return;
+    }
+
+    Plasma::DataEngine::Data results;
+    results = GMailAtomFeedParser::parseFeed(d->http->readAll());
+    setData(d->request, results);
+
+} // httpRequestFinished()
 
 
 #include "gmailnotifierengine.moc"
