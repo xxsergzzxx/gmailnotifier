23bf0d3bf3ed48f9cb0ba1d658f0ceb32cab7e1d
gcnweb <gcnweb@1a4cf6ec-9064-11dd-be88-6f18bdb9940a>
2008-09-03 20:00:11 +0000

    Build script fixes...

--- trunk/playground/Plasma/GmailNotifier/build.sh
+++ trunk/playground/Plasma/GmailNotifier/build.sh
@@ -4,14 +4,20 @@ function msg {
     echo -e "\n$1"
 }
 
+KDE4PREFIX="$(kde4-config --prefix)"
+
 [ ! -d Build ] && mkdir Build
 cd Build
 
 msg "Running CMake..."
-cmake .. -DCMAKE_INSTALL_PREFIX=$KDEDIR -DCMAKE_BUILD_TYPE=debugfull
+cmake .. -DCMAKE_INSTALL_PREFIX=${KDEDIR:=$KDE4PREFIX} -DCMAKE_BUILD_TYPE=Debugfull
 
 msg "Compiling..."
 make
 
 msg "Installing..."
-make install
+if [ ${KDE4PREFIX:0:${#HOME}} == $HOME ]; then
+    make install
+else
+    sudo make install
+fi
