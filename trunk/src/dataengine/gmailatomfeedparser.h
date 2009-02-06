3cf4ffa846becdeb17988639634def40b8bfcb38
gcnweb <gcnweb@1a4cf6ec-9064-11dd-be88-6f18bdb9940a>
2009-02-06 11:51:52 +0000

    - Update mime-types - Use $Id$'s

--- trunk/src/dataengine/gmailatomfeedparser.h
+++ trunk/src/dataengine/gmailatomfeedparser.h
@@ -1,6 +1,8 @@
 /*
 ** Copyright (C) 2008-2009 Gilles CHAUVIN <gcnweb+gmailnotifier@gmail.com>
 **
+** $Id$
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2  of  the  License, or
